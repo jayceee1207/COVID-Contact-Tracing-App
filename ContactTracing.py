290ef906d5dd28861d3b6fc2b3f73054ded08fc8
jayceee1207 <ablayjc7@gmail.com>
2023-07-18 07:48:51 +0800

    Pseudocode of the program

--- ContactTracing.py
+++ ContactTracing.py
@@ -2,3 +2,70 @@
 #BS Computer Engineering 1-5
 #Final Project: COVID Contact Tracing App
 
+
+#PSEUDOCODE
+
+#Create class ContactTracing
+
+    #Add file and its path
+
+    #Create Method: save to file 
+
+    #Create Method: save as file
+
+        #Save contact information
+
+    #Create Method: add contact
+
+        #Infomation for Last Name
+
+        #Infomation for First Name
+
+        #Infomation for Address
+
+        #Infomation for Email-Address
+
+        #Infomation for Contact Number
+
+        #Infomation for Age
+
+        #Infomation for Date
+
+        #Infomation for Time
+
+    #Create Method:save contact
+
+        #add error handlings for the information listed by the user.
+
+    #Create Method: edit contact
+
+        #select entry number from the list
+
+    #Create Method: update contacts
+
+        #Infomation for Last Name
+
+        #Infomation for First Name
+
+        #Infomation for Address
+
+        #Infomation for Email-Address
+
+        #Infomation for Contact Number
+
+        #Infomation for Age
+
+        #Infomation for Date
+
+        #Infomation for Time
+
+    #Create Method: save_edit 
+
+    #Create Method: delete contact
+
+    #Create Method: view contact
+
+    #Create Method: search contact
+
+        #search criteria: 
+
