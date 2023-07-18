e1da6736aa5b258d38da6270dcb02fd61089a290
jayceee1207 <ablayjc7@gmail.com>
2023-07-18 11:19:49 +0800

    save to file method

--- ContactTracing.py
+++ ContactTracing.py
@@ -3,6 +3,9 @@
 #Final Project: COVID Contact Tracing App
 
 import tkinter as tk
+from tkinter import messagebox, filedialog
+import csv 
+import re 
 
 #PSEUDOCODE
 
@@ -16,9 +19,34 @@ class ContactTracing:
         self.window.configure(bg="green")
         self.window.resizable(True, True)
     #Add file and its path
+    #To open a CSV file where informatio of user will be stored
+    def select_file(self):
+        file_path = filedialog.askopenfilename(title="Select Existing File", filetypes=[("CSV Files", "*.csv")])
+        if file_path:
+            try:
+                with open(file_path, mode="r") as file:
+                    reader = csv.reader(file)
+                    contacts = list(reader)
+                    self.entries.extend(contacts)  # Add contacts to the entries list
+                    self.file_path = file_path
+                    messagebox.showinfo("Success", "File added successfully.")
+            except FileNotFoundError:
+                messagebox.showerror("File Not Found", "The selected file does not exist.")
+            except Exception as e:
+                messagebox.showerror("Error", f"Failed to add file: {str(e)}")
 
     #Create Method: save to file 
-
+    def save_to_file(self):
+        if not self.file_path:
+            self.save_as_file() #We will make method for save files
+        else:
+            try:
+                with open(self.file_path, "w", newline="") as file:
+                    writer = csv.writer(file)
+                    writer.writerows(self.entries)
+                messagebox.showinfo("Success", "Contact Information was saved successfully.")
+            except IOError:
+                messagebox.showerror("Error", "Failed to save contact information to file.")
     #Create Method: save as file
 
         #Save contact information
